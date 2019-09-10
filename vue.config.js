954c0f831714d6dcbffd33519e15f767aab1ad39
Toretto5060 <877692526@qq.com>
2019-09-10 13:56:25 +0800

    去除eslint严格模式

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
   // 将构建好的文件输出到哪里
   outputDir: process.env.NODE_ENV === 'production' ? 'dist_pro' : 'dist_dev',
-
+  lintOnSave:false, //关闭eslint警告
   devServer: {
     // host: 'localhost',
     port: 6060,
